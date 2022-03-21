db68d3d5d674245a6383ed43c3b1f2401302917a
Dan Geabunea <dangeabunea@hotmail.com>
2022-03-21 14:31:04 +0200

    Demo 1 ready

--- 3-lsp/demo/isp-before/src/main/java/AircraftLabel.java
+++ 3-lsp/demo/isp-before/src/main/java/AircraftLabel.java
@@ -3,7 +3,7 @@ import java.util.List;
 public interface AircraftLabel {
     String getDisplayValue();
     String getSelectedLabelColor();
-    void onFLightLabelClick();
+    void onFlightLabelClick();
     void onLabelDoubleClick();
     void onLabelTouched();
     void issueCommand(String id, List<String> commandArgs);
