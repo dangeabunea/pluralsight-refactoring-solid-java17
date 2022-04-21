9a0fef2f8400dcd608764e3a5e5049cffa7092de
Dan Geabunea <dangeabunea@hotmail.com>
2022-04-21 06:47:49 +0300

    Completed SRP Checkpoint 1 before

--- 1-srp/checkpoints/checkpoint-1/before/src/test/java/ClassTests.java
+++ 1-srp/checkpoints/checkpoint-1/before/src/test/java/ClassTests.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class FileTests {
+public class ClassTests {
     @Test
     public void getDiscountPriceMethodExists() {
         var getDiscountMethod = TestHelpers.getMethod("Product", "getDiscountPrice");
